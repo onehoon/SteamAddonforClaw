3c6e211b4e2a88db7cc6fd8b87fd217173d1b987
onehoon <onehoon@gmail.com>
2026-08-23 18:47:18 +0900

    Link Korean How to Use wiki page

--- src/SteamInputAddonforClaw.UI/Views/HowToUsePage.xaml.cs
+++ src/SteamInputAddonforClaw.UI/Views/HowToUsePage.xaml.cs
@@ -8,7 +8,7 @@ public sealed partial class HowToUsePage : UserControl
     internal const string EnglishDocumentationUrl =
         "https://github.com/onehoon/SteamInputAddonforClaw#readme";
     internal const string KoreanDocumentationUrl =
-        "https://github.com/onehoon/SteamInputAddonforClaw#readme";
+        "https://github.com/onehoon/SteamInputAddonforClaw/wiki/한국어-사용-가이드";
 
     private bool _activated;
 
