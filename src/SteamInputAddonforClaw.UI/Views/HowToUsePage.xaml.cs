48ce51b9c629395d5c2eef762b962a470b193ad4
onehoon <onehoon@gmail.com>
2026-08-23 17:53:55 +0900

    Add README-based How to Use page

--- src/SteamInputAddonforClaw.UI/Views/HowToUsePage.xaml.cs
+++ src/SteamInputAddonforClaw.UI/Views/HowToUsePage.xaml.cs
@@ -1,11 +1,30 @@
 using Microsoft.UI.Xaml.Controls;
+using System.Globalization;
 
 namespace SteamInputAddonforClaw.Views;
 
 public sealed partial class HowToUsePage : UserControl
 {
+    internal const string EnglishDocumentationUrl =
+        "https://github.com/onehoon/SteamInputAddonforClaw#readme";
+    internal const string KoreanDocumentationUrl =
+        "https://github.com/onehoon/SteamInputAddonforClaw#readme";
+
+    private bool _activated;
+
     public HowToUsePage()
     {
         InitializeComponent();
+    }
+
+    internal void Activate()
+    {
+        if (_activated) return;
+
+        _activated = true;
+        var documentationUrl = CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "ko"
+            ? KoreanDocumentationUrl
+            : EnglishDocumentationUrl;
+        DocumentationWebView.Source = new Uri(documentationUrl);
     }
 }
