c5ed14bded8576642a0fe0d7e0e714ed26c016ab
onehoon <onehoon@gmail.com>
2026-09-12 19:37:09 +0900

    Refine battery charge limit card layout

--- tests/SteamInputAddonforClaw.Tests/UiArchitectureTests.cs
+++ tests/SteamInputAddonforClaw.Tests/UiArchitectureTests.cs
@@ -170,6 +170,10 @@ public sealed class UiArchitectureTests
         Assert.Contains("<FontIcon Glyph=\"&#xE86B;\" />", xaml, StringComparison.Ordinal);
         Assert.Contains("ToggleSwitch x:Name=\"BatteryChargeLimitEnabledToggleSwitch\"", xaml, StringComparison.Ordinal);
         Assert.Contains("Slider x:Name=\"BatteryChargeLimitSlider\"", xaml, StringComparison.Ordinal);
+        Assert.Contains("TextBlock x:Name=\"BatteryChargeLimitValueText\" MinWidth=\"64\"", xaml, StringComparison.Ordinal);
+        Assert.Contains("ToggleSwitch x:Name=\"BatteryChargeLimitEnabledToggleSwitch\" Grid.Column=\"2\"", xaml, StringComparison.Ordinal);
+        Assert.Contains("TextBlock x:Name=\"BatteryChargeLimitStatusText\" Grid.Row=\"1\" Grid.Column=\"1\"", xaml, StringComparison.Ordinal);
+        Assert.Contains("HorizontalAlignment=\"Stretch\" TextAlignment=\"Center\"", xaml, StringComparison.Ordinal);
         Assert.DoesNotContain("SettingsExpander x:Name=\"BatteryChargeLimitCard\"", xaml, StringComparison.Ordinal);
         Assert.Contains("CaptureBatteryChargeLimitAsync", codeBehind, StringComparison.Ordinal);
         Assert.Contains("SetDeviceBatteryChargeLimitPercentAsync", codeBehind, StringComparison.Ordinal);
